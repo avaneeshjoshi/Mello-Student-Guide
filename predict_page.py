5c74f93b25a3bb734573d86a3d6b9c66cd5ba305
avaneeshjoshi <78191094+avaneeshjoshi@users.noreply.github.com>
2022-12-04 15:18:32 -0800

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
-        data = pickle.load(file)
+    file = open('saved_steps.pkl', 'rb')
+    data = pickle.load(file)
     return data
 
 data = load_model()
